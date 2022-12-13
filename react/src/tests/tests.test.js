f0be2ca4644aeda7cd45cd4b4e7a8c4d76563b8a
bearlogin <bearlogin@mail.ru>
2022-12-13 20:18:40 +0300

    refactor

--- react/src/tests/tests.test.js
+++ react/src/tests/tests.test.js
@@ -1,23 +1,27 @@
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, cleanup, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-// import { store } from '../store';
+import {store} from '../store';
 import CreateBookForm from '../components/Books/CreateBookForm';
 
+jest.mock('../store',() => {
+    return {store: { getState:jest.fn(), dispatch: jest.fn(), subscribe: jest.fn() }}
+});
+
+
 describe('Test CreateBookForm', () => {
-    const dispatchMock = jest.fn()
-    const jestSubscribe = jest.fn()
-    let store = { getState:jest.fn(), dispatch: dispatchMock, subscribe: jestSubscribe }
     beforeEach(() => {
-        store = { getState:jest.fn(), dispatch: dispatchMock, subscribe: jestSubscribe }
-    })
-    it('tests create book', async () => {
-        const utils = render(
+        cleanup();
+        render(
             <Provider store={store}>
                 <CreateBookForm />
             </Provider>)
-        const bookName = await utils.findByLabelText('Book Title');
-        const bookAuthor = await utils.findByLabelText('Book Author');
-        const createButton = await utils.findByTestId('create_book_button');
+       jest.clearAllMocks();
+    })
+    it('tests create book', async () => {
+
+        const bookName = await screen.findByLabelText('Book Title');
+        const bookAuthor = await screen.findByLabelText('Book Author');
+        const createButton = await screen.findByTestId('create_book_button');
         fireEvent.change(bookName, { target: { value: 'Forest Gymp' } });
         fireEvent.change(bookAuthor, { target: { value: 'Pushkin' } });
 
@@ -25,22 +29,18 @@ describe('Test CreateBookForm', () => {
         expect(bookAuthor.value).toBe('Pushkin');
 
         fireEvent.click(createButton);
-        expect(dispatchMock).toBeCalledWith({ type: "CREATE_BOOK", payload: {
+        expect(store.dispatch).toBeCalledWith({ type: "CREATE_BOOK", payload: {
                 title: 'Forest Gymp',
                 author: 'Pushkin',
             }});
         expect(bookName.value).toBe('');
-        expect(bookAuthor.value).toBe('');        
+        expect(bookAuthor.value).toBe('');
     })
 
     it('tests error message', async () => {
-        const utils = render(
-            <Provider store={store}>
-                <CreateBookForm />
-            </Provider>)
-        const bookName = await utils.findByLabelText('Book Title');
-        const bookAuthor = await utils.findByLabelText('Book Author');
-        const createButton = await utils.findByTestId('create_book_button');
+        const bookName = await screen.findByLabelText('Book Title');
+        const bookAuthor = await screen.findByLabelText('Book Author');
+        const createButton = await screen.findByTestId('create_book_button');
         fireEvent.change(bookName, { target: { value: '' } });
         fireEvent.change(bookAuthor, { target: { value: '' } });
 
@@ -49,10 +49,10 @@ describe('Test CreateBookForm', () => {
 
         fireEvent.click(createButton);
 
-        const bookNameError = utils.getByTestId('book-name-error-message')
-        const authorNameError = utils.getByTestId('book-author-error-message')
+        const bookNameError = screen.getByTestId('book-name-error-message')
+        const authorNameError = screen.getByTestId('book-author-error-message')
 
-        expect(dispatchMock).not.toBeCalled()
+        expect(store.dispatch).not.toBeCalled()
 
         expect(bookNameError).toBeInTheDocument();
         expect(authorNameError).toBeInTheDocument();
